9f96546254f5b0f40d372b23576065951f9c03e3
Samphors Khlok <khlok.samphors@gmail.com>
2017-09-11 00:56:24 -0500

    Update Code
    - add CSRF
    - add express-validator

--- HW6/app.js
+++ HW6/app.js
@@ -5,15 +5,34 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var lessMiddleware = require('less-middleware');
+var validator = require('express-validator');
+var csrf = require('csurf');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var newsletter = require('./routes/newsletter');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+//set development env
+app.set('env','development');
+console.info(app.get('env'));
+
+//enable and allow trust proxy
+app.enable('trust proxy');
+app.set('trust proxy', true);
+
+//hide powered by Express
+app.set('x-powered-by', false);
+
+// case sensitive routing
+app.enable('case sensitive routing');
+//strict routing
+app.set('strict routing', true);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -23,9 +42,17 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(lessMiddleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(validator());
+app.use(csrf({cookie:true}));
+
+app.use(function(req,res,next){
+  res.locals.csrftoken = req.csrfToken();
+  next();
+})
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/newsletter',newsletter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ HW6/routes/newsletter.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('newsletter', { title: 'Newsletter Page', csrftoken:  req.csrfToken()});
+});
+
+router.post('/addEmail', function(req, res, next) {
+    req.assert('email','email is required').notEmpty();
+
+    var errors = req.validationErrors();
+    if(errors){
+        res.render('error', {
+            message: 'field email is required',
+            error : {
+                status: 'ERROR',
+                stack: JSON.stringify(errors)
+            }
+        } );
+    }else{
+        //console.log(req.body);
+        var email = req.body.email;
+        fs.appendFile('subscribers.txt', email, function (err) {
+            next(err);
+        });
+
+        res.render('thankyou', { title:'Thanks You Page', email: req.body.email });
+    }
+
+});
+
+module.exports = router;
